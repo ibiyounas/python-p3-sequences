fce3e46a1eb6bb0b1f3648e91e895c80632ead2c
Ibrahem Younas <younasibrahem@gmail.com>
2024-05-26 15:48:32 -0400

    passes tests

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
+    list = []
+    if length > 0:
+        list.append(0)
+    if length >=2:
+        list.append(1)
+        for i in range(2, length):
+            list.append(list[-2] + list[-1])
+    print(list)
+        
     pass
